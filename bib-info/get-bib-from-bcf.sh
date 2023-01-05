f6785732490dfbfc8befeb95cfaa63e083d41a03
njleach <njleach.1@gmail.com>
2023-01-05 16:09:02 +0000

    Submitted minor corrections copy

--- bib-info/get-bib-from-bcf.sh
+++ bib-info/get-bib-from-bcf.sh
@@ -5,3 +5,5 @@ cd bib-info
 bib2xml Oxford_Thesis_biber.bib > Oxford_Thesis_biber.xml
 xml2ris Oxford_Thesis_biber.xml > Oxford_Thesis_biber.ris
 python clean_ris.py
+ris2xml Oxford_Thesis_biber_clean.ris > Oxford_Thesis_biber_clean.xml
+xml2bib Oxford_Thesis_biber_clean.xml > Oxford_Thesis_biber_clean.bib
